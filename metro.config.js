463b6ac9a608a0bd47f77e70bc153c0e526869a5
toyo1621 <toyo1621@gmail.com>
2025-12-13 18:25:11 +0900

    Complete rebuild with all features: strawberry juice, SVG islands, and Supabase rankings

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Webビルド用の設定
-config.resolver.unstable_enablePackageExports = true;
-
-module.exports = config;
-
